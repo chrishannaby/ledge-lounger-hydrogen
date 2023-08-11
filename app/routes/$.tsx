aefad9b1a81a9cf35210817936831676d789434e
chrishannaby <chrishannaby@gmail.com>
2023-08-11 10:41:34 -0600

    add proxy

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -1,7 +1,105 @@
-import type {LoaderArgs} from '@shopify/remix-oxygen';
+import type {LoaderArgs, ActionArgs} from '@shopify/remix-oxygen';
 
-export async function loader({request}: LoaderArgs) {
-  throw new Response(`${new URL(request.url).pathname} not found`, {
-    status: 404,
+const config = {
+  removeNoIndex: true, // Set to false if you want to respect robots noindex tags
+  updateCanonical: true, // Set to false if you want to respect canonical meta tags
+  ignoreRedirects: true, // Set to false if you aren't redirecting to Hydrogen in your theme
+};
+
+export async function action({context, request}: ActionArgs) {
+  const {
+    shop: {
+      primaryDomain: {url},
+    },
+  } = await context.storefront.query(
+    `#graphql
+      query {
+        shop {
+          primaryDomain {
+            url
+          }
+        }
+      }
+    `,
+    {
+      cache: context.storefront.CacheLong(),
+    },
+  );
+
+  const {pathname, search} = new URL(request.url);
+  const newUrl = url + pathname + search;
+
+  const newHeaders = new Headers({
+    ...request.headers,
+    'X-Shopify-Client-IP': request.headers.get('X-Shopify-Client-IP') || '',
+    'X-Shopify-Client-IP-Sig':
+      request.headers.get('X-Shopify-Client-IP-Sig') || '',
+    'User-Agent': 'Hydrogen',
   });
+
+  const newRequest = new Request(newUrl, {headers: newHeaders});
+  return await fetch(newRequest);
+}
+
+export async function loader({request, context}: LoaderArgs) {
+  const {
+    shop: {
+      primaryDomain: {url},
+    },
+  } = await context.storefront.query(
+    `#graphql
+      query {
+        shop {
+          primaryDomain {
+            url
+          }
+        }
+      }
+    `,
+    {
+      cache: context.storefront.CacheLong(),
+    },
+  );
+
+  const {origin, pathname, search} = new URL(request.url);
+
+  const customHeaders = new Headers({
+    'X-Shopify-Client-IP': request.headers.get('X-Shopify-Client-IP') || '',
+    'X-Shopify-Client-IP-Sig':
+      request.headers.get('X-Shopify-Client-IP-Sig') || '',
+    'User-Agent': 'Hydrogen',
+  });
+
+  const response = await fetch(url + pathname + search, {
+    headers: customHeaders,
+  });
+
+  const data = await response.text();
+
+  const processedData = data
+    .replace(
+      /<meta.*name="robots".*content="noindex.*".*>|<link.*rel="canonical".*href=".*".*>|("monorailRegion":"shop_domain")|<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi,
+      (match) => {
+        if (match.startsWith('<meta') && config.removeNoIndex) return '';
+        if (match.startsWith('<link') && config.updateCanonical)
+          return match.replace(url, origin);
+        if (match.startsWith('"monorailRegion"'))
+          return '"monorailRegion":"global"';
+        if (match.startsWith('<script') && config.ignoreRedirects)
+          return match.replace(/window\.location\.replace\([^)]*\);?/g, '');
+        return match;
+      },
+    )
+    .replace(new RegExp(url, 'g'), origin);
+
+  const status = /<title>(.|\n)*404 Not Found(.|\n)*<\/title>/i.test(data)
+    ? 404
+    : response.status;
+
+  const headers = new Headers(response.headers);
+  headers.set('content-type', 'text/html');
+  headers.delete('content-encoding');
+  headers.set('Cache-Control', 'public, max-age=1, must-revalidate=9');
+
+  return new Response(processedData, {status, headers});
 }
